c653a1d313d0bc4c74a9e22cc395c6e72d6f2c5c
tHere1sh0p3 <netherlandsh0p3@gmail.com>
2025-03-21 04:49:24 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="assets/css/main.css">
+    <title>РКЦ | Боровичи</title>
+</head>
+
+<body class="wrapper">
+    <div>
+        <header class="container">
+            <span class="logo">РКЦ | Боровичи</span>
+            <nav>
+                <ul>
+                    <li class="active"><a href="#">Главная</a></li>
+                    <li><a href="#">О нас</a></li>
+                    <li><a href="#">GitHub</a></li>
+                    <li class="btn"><a href="#">Контакты</a></li>
+                </ul>
+            </nav>
+        </header>
+        <div class="hero most">
+            <div class="hero---info text">
+                <h2>УП 05 на тему:</h2>
+                <h1>"Информационно-справочная система по культурно-развлекательным центрам"</h1>
+                <p>Сайт о самых популярных культурно-развлекательных центров в нашем любимом городе Боровичи!</p>
+                <button class="">Документация</button>
+                
+            </div>
+        </div>
+        <div class="social-icons">
+                    <div class="icon">
+                        <a class="a-center" href="vokzal.html">
+                            <img src="assets/img/vokzal-icon.png">
+                        </a>
+                    </div>
+                    <div class="icon">
+                        <a class="a-center" href="vokzal.html">
+                            <img src="assets/img/olimp-icon.png">
+                        </a>
+                    </div>
+                    <div class="icon">
+                        <a class="a-center" href="vokzal.html">
+                            <img src="assets/img/uasdiba-icon.png">
+                        </a>
+                    </div>
+                </div>
+    </div>
+</body>
+
+</html>
